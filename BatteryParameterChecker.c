286a76cf8f8120b5be8b35f87ccffa797e91abab
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-08-19 15:11:59 +0530

    Update BatteryParameterChecker.c

--- BatteryParameterChecker.c
+++ BatteryParameterChecker.c
@@ -3,7 +3,7 @@
 
 static int Check_OutOfRange(float value, float min_value, float max_value, enum ParameterType parameter_type) {
     if (is_OutOfRange(value, min_value, max_value)) {
-        print_message(0, parameter_type);
+        PrintMessage(0, parameter_type);
         return 1;
     }
     return 0;
@@ -11,13 +11,13 @@ static int Check_OutOfRange(float value, float min_value, float max_value, enum 
 
 static void Check_LowerLimit(float value, float min_value, float tolerance, enum ParameterType parameter_type) {
     if (is_NearLowerLimit(value, min_value, tolerance)) {
-        print_message(1, parameter_type);
+        PrintMessage(1, parameter_type);
     }
 }
 
 static void Check_UpperLimit(float value, float max_value, float tolerance, enum ParameterType parameter_type) {
     if (is_NearUpperLimit(value, max_value, tolerance)) {
-        print_message(2, parameter_type);
+        PrintMessage(2, parameter_type);
     }
 }
 
