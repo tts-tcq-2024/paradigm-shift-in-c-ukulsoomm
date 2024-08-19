6f55a40a97a42fac6f950d173052b590e9c4d059
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-08-19 15:24:50 +0530

    Update test_BMS.c

--- test_BMS.c
+++ test_BMS.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include "BMS_main.h"
 
+void test_battery(float temperature, float soc, float charge_rate, int language) {
+    Set_Language(language);
+    if (BatteryIsOK(temperature, soc, charge_rate)) {
+        printf("Battery parameters are within range.\n");
+    } else {
+        printf("Battery parameters are out of range.\n");
+    }
+}
