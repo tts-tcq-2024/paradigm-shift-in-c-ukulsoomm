a3933be64726b2cfcb8d2cd582444ae1dd6ff263
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-08-19 15:31:22 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,36 +1,21 @@
 #include <stdio.h>
-#include <assert.h>
-#include <stdbool.h>
+#include "Battery_main.h"
+#include "test_Battery.h"
 
-bool temperratureIsOk(float temperature){
-if(temperature < 0 || temperature > 45){
-  printf("Temperature out of range!\n");
-  return 0;
-  }
-  return 1;
-}
-
-bool SocIsOk(float soc){
-if(soc < 20 || soc > 80) {
-  printf("State of Charge out of range!\n");
-  return 0;
-  }
-  return 1;
-}
-
-bool ChargeRateIsOk(float chargeRate){
-if(chargeRate > 0.8) {
-  printf("Charge Rate out of range!\n");
-  return 0;
-  }
-  return 1;
-}
- 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  return temperratureIsOk(temperature) && SocIsOk(soc) && ChargeRateIsOk(chargeRate);
-}
-
-int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+int main() 
+{
+    // Test cases
+    printf("Testing in English:\n");
+    test_battery(25, 70, 0.7, ENGLISH);
+    test_battery(50, 85, 0, ENGLISH);
+    test_battery(22, 76, 0.78, ENGLISH);
+    test_battery(3, 21, 0.05, ENGLISH);
+    
+    printf("\nTesting in German:\n");
+    test_battery(25, 70, 0.7, GERMAN);
+    test_battery(50, 85, 0, GERMAN);
+    test_battery(22, 76, 0.78, GERMAN);
+    test_battery(3, 21, 0.05, GERMAN);
+    
+    return 0;
 }
