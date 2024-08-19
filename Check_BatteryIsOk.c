d7eb952ef41814a749fb4cc2530d49c3eedd8fed
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-08-19 15:22:56 +0530

    Update Check_BatteryIsOk.c

--- Check_BatteryIsOk.c
+++ Check_BatteryIsOk.c
@@ -4,7 +4,7 @@
 int BatteryIsOk(float temperature, float soc, float charge_rate) 
 {
     float TemperatureTolerance = 2.25; // 5% of upper limit 45
-    float SoCTolerance = 4;            // 5% of upper limit 80
+    float SoCTolerance = 4; // 5% of upper limit 80
     float ChargeRateTolerance = 0.04; // 5% of upper limit 0.8
   
     return (check_parameter(temperature, 0, 45, TemperatureTolerance, TEMPERATURE) && check_parameter(soc, 20, 80, SoCTolerance, SOC) && check_parameter(charge_rate, 0, 0.8, ChargeRateTolerance, CHARGE_RATE));
