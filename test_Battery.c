b0d2c93456dfe4f0b92190787a381731955400aa
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-08-19 15:40:52 +0530

    Update test_Battery.c

--- test_Battery.c
+++ test_Battery.c
@@ -3,7 +3,7 @@
 
 void test_Battery(float temperature, float soc, float charge_rate, int language) {
     Set_Language(language);
-    if (BatteryIsOK(temperature, soc, charge_rate)) {
+    if (BatteryIsOk(temperature, soc, charge_rate)) {
         printf("Battery parameters are within range.\n");
     } else {
         printf("Battery parameters are out of range.\n");
