a56a0d4b644d128f2d7c5edfebdbf4e1728976f5
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-08-19 15:08:29 +0530

    Update TolerenceChecker.c

--- TolerenceChecker.c
+++ TolerenceChecker.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int is_value_out_of_range(float value, float min_value, float max_value) {
+    return (value < min_value || value > max_value);
+}
+
+int is_value_near_lower_limit(float value, float min_value, float tolerance) {
+    return (value < min_value + tolerance);
+}
+
+int is_value_near_upper_limit(float value, float max_value, float tolerance) {
+    return (value > max_value - tolerance);
+}
