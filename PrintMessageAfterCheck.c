f73c678be3fa800a7783e8e48dc261178a4fda1a
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-08-19 15:35:13 +0530

    Update PrintMessageAfterCheck.c

--- PrintMessageAfterCheck.c
+++ PrintMessageAfterCheck.c
@@ -1,5 +1,5 @@
 #include "Battery_main.h"
 
 void PrintMessage(int message_index, enum ParameterType parameter_type) {
-    printf("%s\n", messages[current_language][parameter_type][message_index]);
+    printf("%s\n", messages[Current_Language][parameter_type][message_index]);
 }
