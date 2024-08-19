daf72433fa07d423374cfc1f070f2c46dd78e441
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-08-19 15:50:14 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,16 +6,16 @@ int main()
 {
     // Test cases
     printf("Testing in English:\n");
-    test_battery(25, 70, 0.7, ENGLISH);
-    test_battery(50, 85, 0, ENGLISH);
-    test_battery(22, 76, 0.78, ENGLISH);
-    test_battery(3, 21, 0.05, ENGLISH);
+    test_Battery(25, 70, 0.7, ENGLISH);
+    test_Battery(50, 85, 0, ENGLISH);
+    test_Battery(22, 76, 0.78, ENGLISH);
+    test_Battery(3, 21, 0.05, ENGLISH);
     
     printf("\nTesting in German:\n");
-    test_battery(25, 70, 0.7, GERMAN);
-    test_battery(50, 85, 0, GERMAN);
-    test_battery(22, 76, 0.78, GERMAN);
-    test_battery(3, 21, 0.05, GERMAN);
+    test_Battery(25, 70, 0.7, GERMAN);
+    test_Battery(50, 85, 0, GERMAN);
+    test_Battery(22, 76, 0.78, GERMAN);
+    test_Battery(3, 21, 0.05, GERMAN);
     
     return 0;
 }
