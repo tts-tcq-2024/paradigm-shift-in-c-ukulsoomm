3f1d7814898cfd63ca36de46a1fbd2eab913eb74
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-08-19 15:07:03 +0530

    Update BatteryParameterChecker.h

--- BatteryParameterChecker.h
+++ BatteryParameterChecker.h
@@ -1,8 +1,6 @@
 #ifndef BATTERY_PARAMETER_CHECKER
 #define BATTERY_PARAMETER_CHECKER
 
-#include "Battery.h"
-
-int check_parameter(float value, float min_value, float max_value, float tolerance, enum ParameterType parameter_type);
+int ParameterCheck(float value, float min_value, float max_value, float tolerance, enum ParameterType parameter_type);
 
 #endif // BATTERY_PARAMETER_CHECKER
